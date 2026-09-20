321a80cd0f07598e7829c6bc876a3fa7f0b3eb24
Blotto81 <thorsten-pohl@gmx.de>
2026-09-20 15:13:48 +0200

    Add Homer to score 5 and 8 audio pools

--- modules/dart-caller/dart-caller-audio.js
+++ modules/dart-caller/dart-caller-audio.js
@@ -1,6 +1,6 @@
 (() => {
   const basePath = "modules/dart-caller/audio";
-  const audioVersion = "53";
+  const audioVersion = "54";
   const asset = filename => `${basePath}/${filename}?v=${audioVersion}`;
   const alfSeven = asset("score-7-fun-alf-01.wav");
   const voigt = (score, takes = [1]) => takes.map(
@@ -44,7 +44,9 @@
   turnScores[7] = [...turnScores[7], asset("score-7-fun-seven-days-01.wav"), alfSeven];
   turnScores[16] = [...turnScores[16], asset("score-16-fun-albundy-01.wav")];
   turnScores[20] = [...turnScores[20], asset("score-20-fun-20cm-01.wav")];
-  turnScores[32] = [...turnScores[32], asset("score-32-fun-homer-01.wav")];
+  [5, 8, 32].forEach(score => {
+    turnScores[score] = [...turnScores[score], asset(`score-${score}-fun-homer-01.wav`)];
+  });
   turnScores[51] = [...turnScores[51], asset("score-51-fun-playboy-01.wav")];
 
   const specialCalls = {
